78628251d5f36330f897ceb204dc6a4697dc709e
iydia <lydia.he23@ycdsbk12.ca>
2023-01-09 17:52:03 -0500

    um

---
+++ src/cpt/Main.java
@@ -0,0 +1,20 @@
+package cpt;
+
+import java.io.BufferedReader;
+import java.io.*;
+import java.io.FileReader;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        BufferedReader data = new BufferedReader(new FileReader("income-inequality-before-and-after-taxes.csv"));
+
+    }
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+
+    }
+}
