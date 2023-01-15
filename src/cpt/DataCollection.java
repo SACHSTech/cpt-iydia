d608b8a68fa1d9d216646f2abbd59c60b1b3d79a
iydia <lydia.he23@ycdsbk12.ca>
2023-01-15 17:29:37 -0500

    gridpane

--- src/cpt/DataCollection.java
+++ src/cpt/DataCollection.java
@@ -2,35 +2,24 @@ package cpt;
 
 import java.io.*;
 import java.io.FileReader;
+import java.util.*;
 
 public class DataCollection{
     public static void main(String[] args) throws IOException{
         int intCount = 0;
-        String strCountry;
-        String strCode;
-        int intYear;
-        double dblMarket;
-        double dblDisposable;
-        String strLine;
 
         BufferedReader file = new BufferedReader(new FileReader("src/cpt/income-inequality-before-and-after-taxes.csv"));
-        Data[] data = new Data[180];
+        ArrayList<Data> data = new ArrayList<Data>();
 
         for(intCount = 0; intCount < 180; intCount++){
-            strLine = file.readLine();
-            String[] records = strLine.split(",");
+            String[] records = file.readLine().split(",");
     
-            strCountry = records[0];
-            strCode = records[1];
-            intYear = Integer.parseInt(records[2]);
-            dblMarket = Double.parseDouble(records[3]);
-            dblDisposable = Double.parseDouble(records[4]);
-
-            Data elements = new Data(strCountry, strCode, intYear, dblMarket, dblDisposable);
-            data[intCount] = elements;
-
-            System.out.println(records[0] + "," + records[1] + "," + records[2] + "," + records[3] + "," + records[4]);
+            Data elements = new Data(records[0], records[1], Integer.parseInt(records[2]), Double.parseDouble(records[3]), Double.parseDouble(records[4]));
+            data.add(elements);
+            //System.out.println(elements.toString());
+            //System.out.println(strCountry + "," + strCode + "," + intYear + "," + dblMarket + "," + dblDisposable);
         }
+        //System.out.println(data);
         file.close();
     }
 }
--- src/cpt/Database.java
+++ src/cpt/Database.java
@@ -1,15 +1,30 @@
 package cpt;
 
+import java.util.ArrayList;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 
 public class Database extends Application{
-
+    /**
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
         launch(args);
     }
@@ -18,21 +33,51 @@ public class Database extends Application{
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Income Inequality Database");
 
-        Menu menu1 = new Menu("Home");
-        Menu menu2 = new Menu("Charts");
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setGridLinesVisible(false);
 
-        MenuItem menuItem1 = new MenuItem("Line Chart");
-        MenuItem menuItem2 = new MenuItem("Bar Chart");
+        Menu home = new Menu("Home");
+        Menu charts = new Menu("Charts");
+
+        MenuItem lineChart = new MenuItem("Line Chart");
+        MenuItem barChart = new MenuItem("Bar Chart");
         
-        menu2.getItems().add(menuItem1);
-        menu2.getItems().add(menuItem2);
+        charts.getItems().add(lineChart);
+        charts.getItems().add(barChart);
 
         MenuBar menuBar = new MenuBar();
-        menuBar.getMenus().add(menu1);
-        menuBar.getMenus().add(menu2);
+        menuBar.getMenus().add(home);
+        menuBar.getMenus().add(charts);
+
+        grid.add(menuBar, 0, 0, 2, 1); //(columnIndex, rowIndex, columnSpan, rowSpan)
         
-        VBox vBox = new VBox(menuBar);
-        Scene scene = new Scene(vBox, 960, 600);
+        lineChart.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Line Chart Pressed");
+                
+                    Text scenetitle = new Text("Welcome");
+                    scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+                    grid.add(scenetitle, 1, 1, 1, 1);
+    
+                    Label userName = new Label("Line Chart Page");
+                    grid.add(userName, 1, 2);
+            }
+        });
+
+        barChart.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Bar Chart Pressed");
+
+                Text test = new Text("Bar Chart Page");
+                grid.add(test, 1, 3, 1, 1);
+            }
+        });
+
+        Scene scene = new Scene(grid, 960, 600);
 
         primaryStage.setScene(scene);
         primaryStage.show();
