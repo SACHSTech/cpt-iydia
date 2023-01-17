aa61ed35dbfdd51f41bc71aebb07d21e0e5f3539
iydia <lydia.he23@ycdsbk12.ca>
2023-01-17 10:05:09 -0500

    filterByCountry method

--- src/cpt/DataCollection.java
+++ src/cpt/DataCollection.java
@@ -1,23 +1,38 @@
 package cpt;
 
 import java.io.*;
-import java.io.FileReader;
 import java.util.*;
 
 public class DataCollection{
-    public static void main(String[] args) throws IOException{
-        int intCount = 0;
+    static ArrayList<Data> byCountry = new ArrayList<Data>();
+    static int[] filteredRows;
 
+    public static void main(String[] args) throws IOException{
         BufferedReader file = new BufferedReader(new FileReader("src/cpt/income-inequality-before-and-after-taxes.csv"));
         ArrayList<Data> data = new ArrayList<Data>();
 
-        for(intCount = 0; intCount < 180; intCount++){
+        for(int i = 0; i < 180; i++){
             String[] records = file.readLine().split(",");
     
             Data elements = new Data(records[0], records[1], Integer.parseInt(records[2]), Double.parseDouble(records[3]), Double.parseDouble(records[4]));
             data.add(elements);
-            System.out.println(elements.toString());
+            //System.out.println(elements.toString());
+            filterByCountry("Canada");
         }
         file.close();
+    }
+
+    public static void filterByCountry(String strCountry) throws IOException{
+        BufferedReader file = new BufferedReader(new FileReader("src/cpt/income-inequality-before-and-after-taxes.csv"));
+
+        for(int i = 0; i < 180; i++){
+            String[] records = file.readLine().split(",");
+
+            if(records[0].equals(strCountry)){
+                Data dataByCountry = new Data(records[0], records[1], Integer.parseInt(records[2]), Double.parseDouble(records[3]), Double.parseDouble(records[4]));
+                //byCountry.add(dataByCountry);
+                System.out.println(dataByCountry.toString());
+            }
+        }
     }
 }
